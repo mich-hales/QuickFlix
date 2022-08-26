0316362367c0c15341bc5182e521f1f3f7abe5f1
nookworth <nooksack@gmail.com>
2022-08-25 20:33:21 -0600

    My fetch request for the movie database.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,10 @@
-var inputForm = document.querySelector('#input-form');
-var key = 'fTuNIcv1ybxefwYo_uLu0rfDNfkG5iNUEnESFcFeJPywZwQXlax6XeBqaQ0g9MnFtfS-beghCMtTzuWBKrB9Cn-sRrfV9yn2bIGptKYTtXC2A5Gx6k5u0EDMSM8GY3Yx'
-  
-
-var getRequest = 'https://thingproxy.freeboard.io/fetch/https://api.yelp.com/v3/businesses/search&key=fTuNIcv1ybxefwYo_uLu0rfDNfkG5iNUEnESFcFeJPywZwQXlax6XeBqaQ0g9MnFtfS-beghCMtTzuWBKrB9Cn-sRrfV9yn2bIGptKYTtXC2A5Gx6k5u0EDMSM8GY3Yx'
-
-
-
-// var apiUrl = 'https://api.yelp.com/v3/businesses/search?term=delis&latitude=37.786882&longitude=-122.399972'
-
-// fetch(apiUrl).then(function (response) {
-//     response.json().then(function(data) {
-//         console.log(data);
-//     })
-// })
-
-
-
-var openTableApi = 'https://opentable.herokuapp.com/api'
-
-inputForm.addEventListener('click', function() {
-    fetch(openTableApi)
-    .then(res => {
-        return res.json()
-    })
-    .then(data => console.log(data))
-    .catch(error => console.log('error'));
-})
-
-// $('#input-form').click(function(){
-//     $.get(apiUrl, function(data, response){
-//       console.log(data, response);
-//    });
-//   });
-
-
- 
+fetch(
+  "https://api.themoviedb.org/3/movie/upcoming?api_key=0d83e0ad9f06857804273761b2c3701a&language=en-US&page=1"
+)
+  .then((res) => {
+    return res.json();
+  })
+  .then((data) => {
+    var movieTitle = data.results[0].title;
+    randomMovieOutput(movieTitle);
+  });
