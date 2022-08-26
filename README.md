15f0bf4e811823bd976ccc48f16ebaeee544ac49
mich-hales <michellehales93@gmail.com>
2022-08-26 10:19:38 -0600

    cleaned up code, added button, hid containers to then show up after, fixed user input to reset value

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# group-project
-My favorite part of javascript is when something clicks.
+# NEED NEW NAME
+
+## Description
+* will describe application here 
+
+... generates random movie for users that are 'about to come out into theaters???' and will provide movie trailers for the individual to watch and create a watch list for the user to add movies to their list. 
+
+Will take the name of the random movie and automatically place it in the input value for the user to choose whether or not they want to add to their movie list. 
+
+If they later decide they want to remove it, the delete button can be clicked and it will be removed from their list. If they want to delete the whole list, they can clear list to start with an empty list. 
+
+
+## Mock-up
+* will need link to deployed website
+* will need link to github
+* will need screen shot of app
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,64 +1,52 @@
-// var inputForm = document.querySelector('#input-form');
+// TO DO:
+// readme file 
+// presentation
+// new name for application
+// random generator for movie api
 
-// using jquery to get api data and display a movie trailer based on input
+
+
+// When 'find me a movie button' is clicked, will get data from movie api and input into this function
 function randomMovieOutput(movie) {
+    // YouTube API key
     var youtubeApiKEy = 'AIzaSyD5JdQlIa-kgrfw-LaRZKnqT5QVKgtvt_o'
-    // var video = '';
     console.log(movie)
-
-    // $('#yt-player').submit(function(event) {
-    //     event.preventDefault();
-   
         //  will get the user input when the item is searched for and add the word trailer to present trailers
         var userInput = movie + 'trailer';
-        // $('#search').val()
-
-        videoSearch(youtubeApiKEy, userInput, 5)
+        // arguments for the function videoSearch
+        videoSearch(youtubeApiKEy, userInput, 3)
     }
 
-    
+    // function to take in arguments to put into the YouTube API to get results
     function videoSearch(key, search, maxResults) {
         // clears out old data
         $('#videos').empty();
-
+        // requesting data from API with results that were chosen in the previous function
         $.get('https://www.googleapis.com/youtube/v3/search?key='+ key
         + '&type=video&part=snippet&maxResults=' + maxResults + '&q=' + search, function(data){
             console.log(data)
-
+            // for each of the YouTube videos, will create an iframe element to display the video
             data.items.forEach(item => {
                 video = `
                 <iframe width="420" height="315" src="http://www.youtube.com/embed/${item.id.videoId}" frameborder="0" allowfullscreen></iframe>
                 `
+                // appends videos to page
                 $('#videos').append(video);
             })
         })
     }
-// }
-
-
-    
-
-// local storage for random movie output -- will chose another if the same
-// storage -- stores movie ideas to a list? 
-// add movies they want to watch to a list 
-// readme file 
-// presentation
-// new name -- preview
-
-
 
 
 // javascript for localstorage to save favorite movies of choice to their watch list
-
 const inputVal = document.querySelector('.userInput');
 const addTaskBtn = document.querySelector('.addListBtn');
 
-
-
+// will use local storage to take input 
 function inputMovieVal(watchlist) {
+    // takes the random movie and puts it into this function to give user an option if they want to add to their list
     inputVal.value = watchlist;
     addTaskBtn.addEventListener('click', function() {
-
+        
         if (inputVal.value != 0) {
             let localItems = JSON.parse(localStorage.getItem('localItem'))
     
@@ -70,11 +58,14 @@ function inputMovieVal(watchlist) {
             taskList.push(inputVal.value)
             localStorage.setItem('localItem', JSON.stringify(taskList))
         }
+        // calls the show list function
         showList();
+        // resets input value 
+        inputVal.value = '';
     })
 }
 
-
+// shows the items on the page
 function showList() {
     let outPut = '';
     let taskListShow = document.querySelector('.watchListItem');  
@@ -88,6 +79,7 @@ function showList() {
         console.log('success')
     }
 
+    // creates html for the data to be placed and also allows for the user to delete an item if wanted. Will call the deleteItem function to execute
     taskList.forEach((data, index) => {
         outPut += `
         <div class="moviesList">
@@ -102,30 +94,42 @@ function showList() {
 }
 showList();
 
-
+// local storage
 function deleteItem(index) {
     let localItems = JSON.parse(localStorage.getItem('localItem'))
+    // will delete the items from the list when x is clicked
     taskList.splice(index, 1);
     localStorage.setItem('localItem', JSON.stringify(taskList));
     showList();
 }
 
+// calls clearTask function (which is called in html document) and will clear local storage and the list
 function clearTask() {
     localStorage.clear();
     showList();
 }
 
 
+// declares variables 
+var generateBtn = document.querySelector('.generateBtn');
+var displayResults = document.querySelector('.display-results');
 
-fetch(
-  "https://api.themoviedb.org/3/movie/upcoming?api_key=0d83e0ad9f06857804273761b2c3701a&language=en-US&page=1"
-)
-  .then((res) => {
-    return res.json();
-  })
-  .then((data) => {
-    var movieTitle = data.results[0].title;
-    randomMovieOutput(movieTitle);
-    console.log(data)
-    inputMovieVal(movieTitle);
-  });
+
+// when 'find me a movie' button is clicked, will generate movie for user
+generateBtn.addEventListener('click', function() {
+    // shows Movie Trailers section and Watch List
+    displayResults.classList.remove('hide')
+
+    fetch(
+        "https://api.themoviedb.org/3/movie/upcoming?api_key=0d83e0ad9f06857804273761b2c3701a&language=en-US&page=1"
+      )
+        .then((res) => {
+          return res.json();
+        })
+        .then((data) => {
+          var movieTitle = data.results[0].title;
+          randomMovieOutput(movieTitle);
+          console.log(data)
+          inputMovieVal(movieTitle);
+        });    
+})
