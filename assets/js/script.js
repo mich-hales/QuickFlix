f73adeb2547b4df283b2c9ac78d5eaceec125c69
mich-hales <michellehales93@gmail.com>
2022-08-25 18:52:29 -0600

    don't merge yet... just updates for the YT API

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,100 @@
 var inputForm = document.querySelector('#input-form');
-var key = 'fTuNIcv1ybxefwYo_uLu0rfDNfkG5iNUEnESFcFeJPywZwQXlax6XeBqaQ0g9MnFtfS-beghCMtTzuWBKrB9Cn-sRrfV9yn2bIGptKYTtXC2A5Gx6k5u0EDMSM8GY3Yx'
-  
-
-var getRequest = 'https://thingproxy.freeboard.io/fetch/https://api.yelp.com/v3/businesses/search&key=fTuNIcv1ybxefwYo_uLu0rfDNfkG5iNUEnESFcFeJPywZwQXlax6XeBqaQ0g9MnFtfS-beghCMtTzuWBKrB9Cn-sRrfV9yn2bIGptKYTtXC2A5Gx6k5u0EDMSM8GY3Yx'
 
 
 
-// var apiUrl = 'https://api.yelp.com/v3/businesses/search?term=delis&latitude=37.786882&longitude=-122.399972'
 
-// fetch(apiUrl).then(function (response) {
-//     response.json().then(function(data) {
-//         console.log(data);
-//     })
-// })
+$(document).ready(function() {
+    var youtubeApiKEy = 'AIzaSyD5JdQlIa-kgrfw-LaRZKnqT5QVKgtvt_o'
+    var video = '';
 
+    $('#yt-player').submit(function(event) {
+        event.preventDefault();
+   
+        //  will get the user input when the item is searched for and add the word trailer to present trailers
+        var userInput = $('#search').val() + 'trailer';
 
-
-var openTableApi = 'https://opentable.herokuapp.com/api'
-
-inputForm.addEventListener('click', function() {
-    fetch(openTableApi)
-    .then(res => {
-        return res.json()
+        videoSearch(youtubeApiKEy, userInput, 5)
     })
-    .then(data => console.log(data))
-    .catch(error => console.log('error'));
+
+    function videoSearch(key, search, maxResults) {
+        // clears out old data
+        $('#videos').empty();
+
+        $.get('https://www.googleapis.com/youtube/v3/search?key='+ key
+        + '&type=video&part=snippet&maxResults=' + maxResults + '&q=' + search, function(data){
+            console.log(data)
+
+            data.items.forEach(item => {
+                video = `
+                <iframe width="420" height="315" src="http://www.youtube.com/embed/${item.id.videoId}" frameborder="0" allowfullscreen></iframe>
+                `
+                $('#videos').append(video);
+            })
+        })
+    }
 })
 
-// $('#input-form').click(function(){
-//     $.get(apiUrl, function(data, response){
-//       console.log(data, response);
-//    });
-//   });
+// local storage for random movie output -- will chose another if the same
+// storage -- stores movie ideas to a list? 
+// add movies they want to watch to a list 
+// readme file 
+// presentation
+// new name -- preview
 
 
- 
+const inputVal = document.querySelector('.inputVal');
+const addTaskBtn = document.querySelector('.addListBtn');
+// const watchList = document.querySelector('.watchList');
+// const watchListItem = document.querySelector('.watchListItem');
+
+addTaskBtn.addEventListener('click', () => {
+    if(inputVal.value != 0) {
+        let localItems = JSON.parse(localStorage.getItem('localItem'))
+
+        if (localItems === null) {
+            taskList = []
+        } else {
+            taskList = localItems;
+        }
+        taskList.push(inputVal.value)
+        localStorage.setItem('localItem', JSON.stringify(taskList))
+    }
+    showList();
+})
+
+
+function showList() {
+    let outPut = '';
+    let taskListShow = document.querySelector('.watchListItem');  
+    let localItems = JSON.parse(localStorage.getItem('localItem'))
+
+    if (localItems === null) {
+        taskList = []
+    } else {
+        taskList = localItems;
+    }
+    taskList.forEach((data, index) => {
+        outPut += `
+        <div class="theMoviesList">
+            <p class="pText">${data}</p>
+            <button class="deleteTask" onClick="deleteItem(${index})">x</button>
+        </div>
+        `
+    });
+    taskListShow.innerHTML = outPut;
+}
+showList();
+
+
+function deleteItem(index) {
+    let localItems = JSON.parse(localStorage.getItem('localItem'))
+    taskList.splice(index, 1);
+    localStorage.setItem('localItem', JSON.stringify(taskList));
+    showList();
+}
+
+function clearTask() {
+    localStorage.clear();
+    showList();
+}
+
